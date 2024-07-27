1d819a837e3214f7f1d2f66a4d8d06f496f501b7
diafernandezso <diafernandezso@unal.edu.co>
2024-07-26 22:30:15 -0500

    Update sumador4bit_tb.v

--- lab002/src/sumador4bit_tb.v
+++ lab002/src/sumador4bit_tb.v
@@ -1,12 +1,14 @@
 `timescale 1ns / 1ps
 module testbench;
-
+		// inicializamos entradas 
    reg [3:0] a, b;      
-   reg cin;
+   reg cin =0;
    wire [3:0] s;       
    wire cout4;
 
+//instanciamos modulos 
 
+// modulo del sumador 4 bits
 sumador4bit dut (
     .b(b),
     .a(a),
@@ -15,18 +17,56 @@ sumador4bit dut (
     .cout4(cout4)
 );
 
-// Inicialización de señales
-  initial 
-  begin
-   	// Caso de prueba : 
-    a = 4'b0010; b = 4'b0001; cin = 1'b0;
-    #100 a = 4'b0110; b = 4'b0001; cin = 1'b0;
-    $display("Suma = %b, cout = %b ", s,cout4);
-    #100 a = 4'b1110; b = 4'b0001; cin = 1'b0;    
-    $display("Suma = %b, cout = %b ", s,cout4);
-    #100 a = 4'b1111; b = 4'b0001; cin = 1'b0;    
-    $display("Suma = %b, cout = %b ", s,cout4);
-   
+wire [0:6] Sseg;
+
+//modulo responsable del 7-segmentos
+
+BCDtoSSeg dut2 (
+	s,
+	Sseg
+);
+
+
+// Damos valores iniciales a las entradas para que los modulos produzcan salidas
+ initial begin
+        // Caso 1
+        #100 a = 4'b0000; b = 4'b0000; cin = 1'b0;  // a = 0, b = 0, resultado = 0, Resultado_en_binario = 0000
+        #101 $display("a = %b, b = %b, Suma = %b, cout = %b", a, b, s, cout4);
+
+        // Caso 2
+        #100 a = 4'b0001; b = 4'b0000; cin = 1'b0;  // a = 1, b = 0, resultado = 1, Resultado_en_binario = 0001
+        #101 $display("a = %b, b = %b, Suma = %b, cout = %b", a, b, s, cout4);
+
+        // Caso 3
+        #100 a = 4'b0010; b = 4'b0011; cin = 1'b0;  // a = 2, b = 3, resultado = 5, Resultado_en_binario = 0101
+        #101 $display("a = %b, b = %b, Suma = %b, cout = %b", a, b, s, cout4);
+
+        // Caso 4
+        #100 a = 4'b1111; b = 4'b0001; cin = 1'b0;  // a = 15, b = 1, resultado = 16, Resultado_en_binario = 0000, cout = 1
+        #101 $display("a = %b, b = %b, Suma = %b, cout = %b", a, b, s, cout4);
+
+        // Caso 5
+        #100 a = 4'b1111; b = 4'b1111; cin = 1'b0;  // a = 15, b = 15, resultado = 30, Resultado_en_binario = 1110, cout = 1
+        #101 $display("a = %b, b = %b, Suma = %b, cout = %b", a, b, s, cout4);
+
+        // Caso 6
+        #100 a = 4'b1111; b = 4'b1111; cin = 1'b1;  // a = 15, b = 15, resultado = 31, Resultado_en_binario = 1111, cout = 1
+        #101 $display("a = %b, b = %b, Suma = %b, cout = %b", a, b, s, cout4);
+
+        // Caso 7
+        #100 a = 4'b0101; b = 4'b1010; cin = 1'b0;  // a = 5, b = 10, resultado = 15, Resultado_en_binario = 1111
+        #101 $display("a = %b, b = %b, Suma = %b, cout = %b", a, b, s, cout4);
+
+        // Caso 8
+        #100 a = 4'b1100; b = 4'b0011; cin = 1'b1;  // a = 12, b = 3, resultado = 16, Resultado_en_binario = 0000, cout = 1
+        #101 $display("a = %b, b = %b, Suma = %b, cout = %b", a, b, s, cout4);
+	
+	
+	
+	
+	
+	
+	
     #100 $finish(); // [stop(), $finish()]
     
 	end
